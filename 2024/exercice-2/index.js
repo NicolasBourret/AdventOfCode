94aabbef329caa829133d65f082700dfa1c875a9
Nicolas Bourret <nicolas.bourret48@gmail.com>
2024-12-09 00:15:28 +0100

    wip  day 2 part 2

--- 2024/exercice-2/index.js
+++ 2024/exercice-2/index.js
@@ -1,19 +1,21 @@
-// import { input } from "./input.js";
-const input = `
-7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9
-`;
+import { input } from "./input.js";
+// const input = `
+// 7 6 4 2 1
+// 1 2 7 8 9
+// 9 7 6 2 1
+// 1 3 2 4 5
+// 8 6 4 4 1
+// 1 3 6 7 9
+// `;
 
 const trimValue = (value) => value.trim();
 const convertInArray = (value, expression) => value.split(expression);
 const convertInNumber = (value) => Number(value);
 const isBetween = (a, b) => Math.abs(a - b) >= 1 && Math.abs(a - b) <= 3;
 const isIncrease = (a, b, c) => a < b && b < c;
+// const isIncrease = (a, b) => a < b;
 const isDecrease = (a, b, c) => a > b && b > c;
+// const isDecrease = (a, b) => a > b;
 const convertInputInArray = (input) => {
   const trimedInput = trimValue(input);
   const inputToArray = convertInArray(trimedInput, /\n/g);
@@ -30,12 +32,14 @@ const convertInputInArray = (input) => {
 //     (isIncrease(a, b, c) || isDecrease(a, b, c))
 //   );
 // };
+
+// 459 > reponse > 395
 const elementIsSafe = (array, start, end) => {
   const slicedArray = array.slice(start, end);
   const [a, b, c] = slicedArray.map(convertInNumber);
+  //   console.log(a, b, c);
   return (
-    isBetween(a, b) &&
-    isBetween(b, c) &&
+    ((isBetween(a, b) && isBetween(b, c)) || isBetween(a, c)) &&
     (isIncrease(a, b, c) || isDecrease(a, b, c))
   );
 };
@@ -59,7 +63,7 @@ const checkReport = (report) => {
       if (currentIndex === levelsArray.length - 1) {
         accumulator = [
           ...accumulator,
-          elementIsSafe(levelsArray, currentIndex - 2) ? "safe" : "unsafe",
+          elementIsSafe(levelsArray, 1 - currentIndex) ? "safe" : "unsafe",
         ];
 
         return accumulator;
@@ -76,23 +80,18 @@ const checkReport = (report) => {
     },
     []
   );
-  //   console.log(inspectedLevels);
-
-  //   const unsafedElements = levelsArray.filter(
-  //     (level, i) => inspectedLevels[i] === "unsafe"
-  //   );
-
-  //   console.log(unsafedElements);
 
   return inspectedLevels.includes("unsafe") ? "unsafe" : "safe";
 };
+
+// console.log(checkReport("1 2 7 8 9"));
 
 const findSafeReports = (input) => {
   const arrayInput = convertInputInArray(input);
   const checkedReports = arrayInput.map(checkReport);
   const safeReports = checkedReports.filter((report) => report === "safe");
 
-  console.log(checkedReports);
+  console.log(safeReports.length);
   return safeReports.length;
 };
 
