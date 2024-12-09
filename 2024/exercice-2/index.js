202be5486a7a468703634ca545964f038ee8467c
Nicolas Bourret <nicolas.bourret48@gmail.com>
2024-12-09 21:09:42 +0100

    day 2 part agaaaaaaaain

--- 2024/exercice-2/index.js
+++ 2024/exercice-2/index.js
@@ -12,10 +12,10 @@ const trimValue = (value) => value.trim();
 const convertInArray = (value, expression) => value.split(expression);
 const convertInNumber = (value) => Number(value);
 const isBetween = (a, b) => Math.abs(a - b) >= 1 && Math.abs(a - b) <= 3;
-const isIncrease = (a, b, c) => a < b && b < c;
-// const isIncrease = (a, b) => a < b;
-const isDecrease = (a, b, c) => a > b && b > c;
-// const isDecrease = (a, b) => a > b;
+// const isIncrease = (a, b, c) => a < b && b < c;
+const isIncrease = (a, b) => a < b;
+// const isDecrease = (a, b, c) => a > b && b > c;
+const isDecrease = (a, b) => a > b;
 const convertInputInArray = (input) => {
   const trimedInput = trimValue(input);
   const inputToArray = convertInArray(trimedInput, /\n/g);
@@ -39,52 +39,72 @@ const elementIsSafe = (array, start, end) => {
   const [a, b, c] = slicedArray.map(convertInNumber);
   //   console.log(a, b, c);
   return (
-    ((isBetween(a, b) || isBetween(b, c)) || isBetween(a, c)) &&
+    (isBetween(a, b) || isBetween(b, c) || isBetween(a, c)) &&
     (isIncrease(a, b, c) || isDecrease(a, b, c))
   );
 };
 
 const checkReport = (report) => {
-  const levelsArray = convertInArray(report, " ");
+  const levelsArray = convertInArray(report, " ").map(convertInNumber);
 
-  const inspectedLevels = levelsArray.reduce(
-    (accumulator, currentValue, currentIndex) => {
-      if (currentIndex === 0) {
-        accumulator = [
-          ...accumulator,
-          elementIsSafe(levelsArray, currentIndex, currentIndex + 3)
-            ? "safe"
-            : "unsafe",
-        ];
+  // PART 1
 
-        return accumulator;
-      }
+  /**
+   * 7, 6, 4, 2, 1
+   * 7 est safe car:
+   *  1 <= 7 - 6 <= 3 et 7 > 6 > 4
+   *
+   * 6 est safe car:
+   *  7 > 6 > 4 et 1 <= 7 - 6 <= 3 et 1 <= 6 - 4 <= 3
+   *
+   * 4 est safe car:
+   *  6 > 4 > 2 et 1 <= 6 - 4 <= 3 et 1 <= 4 - 2 <= 3
+   *
+   * 2 est safe car:
+   *  4 > 2 > 1 et 1 <= 4 - 2 <= 3 et 1 <= 2 - 1 <= 3
+   *
+   * 1 est safe car:
+   *  1 <= 2 - 1 <= 3 et 4 > 2 > 1
+   */
+  const inspectedLevels = levelsArray.reduce((acc, value, i) => {
+    if (i === 0) {
+      const a = value;
+      const b = levelsArray[i + 1];
+      const c = levelsArray[i + 2];
+      const safe =
+        ((isIncrease(a, b) && isIncrease(b, c)) ||
+          (isDecrease(a, b) && isDecrease(b, c))) &&
+        isBetween(a, b);
 
-      if (currentIndex === levelsArray.length - 1) {
-        accumulator = [
-          ...accumulator,
-          elementIsSafe(levelsArray, 1 - currentIndex) ? "safe" : "unsafe",
-        ];
+      return [...acc, safe ? "safe" : "unsafe"];
+    }
 
-        return accumulator;
-      }
+    if (i === levelsArray.length - 1) {
+      const a = levelsArray[i - 2];
+      const b = levelsArray[i - 1];
+      const c = value;
+      const safe =
+        ((isIncrease(a, b) && isIncrease(b, c)) ||
+          (isDecrease(a, b) && isDecrease(b, c))) &&
+        isBetween(b, c);
 
-      accumulator = [
-        ...accumulator,
-        elementIsSafe(levelsArray, currentIndex - 1, currentIndex + 2)
-          ? "safe"
-          : "unsafe",
-      ];
+      return [...acc, safe ? "safe" : "unsafe"];
+    }
 
-      return accumulator;
-    },
-    []
-  );
+    const a = levelsArray[i - 1];
+    const b = value;
+    const c = levelsArray[i + 1];
+    const safe =
+      ((isIncrease(a, b) && isIncrease(b, c)) ||
+        (isDecrease(a, b) && isDecrease(b, c))) &&
+      isBetween(b, c) &&
+      isBetween(a, b);
+
+    return [...acc, safe ? "safe" : "unsafe"];
+  }, []);
 
   return inspectedLevels.includes("unsafe") ? "unsafe" : "safe";
 };
-
-// console.log(checkReport("1 2 7 8 9"));
 
 const findSafeReports = (input) => {
   const arrayInput = convertInputInArray(input);
