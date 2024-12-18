5bf916732589169c67a5b5583e00917662a41886
Nicolas Bourret <nicolas.bourret48@gmail.com>
2024-12-18 20:53:09 +0100

    2015 day 1

--- 2015/exercice-1/index.js
+++ 2015/exercice-1/index.js
@@ -1,4 +1,5 @@
 import { input } from "./input.js";
+// const input = "()())";
 
 // PART 1
 const start = 0;
@@ -13,3 +14,29 @@ const floor = [...input].reduce((accumulator, currentValue) => {
 
   return accumulator;
 }, start);
+
+// PART 2
+const enterBasement = (array) => {
+  console.log(array.length);
+  let indexBasement = 0;
+  let floor = 0;
+  for (let i = 0; i <= array.length - 1; i++) {
+    if (array[i] === "(") {
+      floor += 1;
+    }
+
+    if (array[i] === ")") {
+      floor -= 1;
+    }
+
+    if (floor === -1) {
+      indexBasement = i;
+      break;
+    }
+  }
+
+  return indexBasement + 1;
+};
+
+console.log(enterBasement([...input]));
+console.log(floor);
